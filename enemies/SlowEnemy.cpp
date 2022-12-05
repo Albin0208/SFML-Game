13b0effe171ac85d6c36461b0a277e573166f102
Albin Dahlén <albin@dahlen.ws>
2022-12-05 16:25:29 +0100

    Added smoother movement for enemy

--- enemies/SlowEnemy.cpp
+++ enemies/SlowEnemy.cpp
@@ -14,20 +14,8 @@ SlowEnemy::SlowEnemy(sf::Vector2f const& position, float speed, sf::Vector2f con
 void SlowEnemy::update(sf::Time const& time, Game& game) {
     sf::Vector2f dir{player_pos - shape.getPosition()};
 
-    if (dir.x > 1)
-        dir.x = 1;
-    if (dir.x < -1)
-        dir.x = -1;
+    dir /= static_cast<float>(sqrt(pow(dir.x, 2) + pow(dir.y, 2)));
 
-    if (dir.y > 1)
-        dir.y = 1;
-    if (dir.y < -1)
-        dir.y = -1;
-
-    // Normalize movement
-    float len{static_cast<float>(sqrt(pow(dir.x, 2) + pow(dir.y, 2)))};
-    if (len > 0.0f)
-        dir * (1.0f / len);
     position += speed * dir * time.asSeconds();
     shape.setPosition(position);
 
