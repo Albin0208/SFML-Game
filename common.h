9044d861e25131d71f890f4a7e1ee2ad2517a039
Albin Dahlén <albin@dahlen.ws>
2022-12-05 08:44:20 +0100

    Added common.h file

---
+++ common.h
@@ -0,0 +1,16 @@
+//
+// Created by albin on 2022-12-05.
+//
+
+#ifndef TDP005_COMMON_H
+#define TDP005_COMMON_H
+
+#include <vector>
+#include <cmath>
+
+#include "SFML/Graphics.hpp"
+
+unsigned int const WIDTH{1024};
+unsigned int const HEIGHT{768};
+
+#endif //TDP005_COMMON_H
