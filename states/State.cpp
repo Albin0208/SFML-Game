45c4183af1939d2e5fc87dc25716561c58cbc5e0
filin <filin764@student.liu.se>
2022-12-12 11:34:18 +0100

    background lag

--- states/State.cpp
+++ states/State.cpp
@@ -42,6 +42,23 @@ void State::run(std::shared_ptr<State> state) {
             }
             continue;
         }
+        sf::Texture BackgroundTexture;
+        sf::Sprite background;
+        sf::Vector2u TextureSize;  //Added to store texture size.
+        sf::Vector2u WindowSize;   //Added to store window size.
+
+        BackgroundTexture.loadFromFile("..//resources//textures//background.png");
+
+        TextureSize = BackgroundTexture.getSize(); //Get size of texture.
+        WindowSize = window.getSize();             //Get size of window.
+
+        float ScaleX = (float) WindowSize.x / TextureSize.x;
+        float ScaleY = (float) WindowSize.y / TextureSize.y;     //Calculate scale.
+
+        background.setTexture(BackgroundTexture);
+        background.setScale(ScaleX, ScaleY);      //Set scale.
+
+        window.draw(background);
 
         state->render(window);
         window.display();
