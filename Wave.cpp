3f92899818c9d7d238fb660d7469d4f1b28b3933
Albin Dahlén <albda746@student.liu.se>
2022-12-13 14:44:42 +0100

    Formatted if statement

--- Wave.cpp
+++ Wave.cpp
@@ -18,26 +18,19 @@ vector<shared_ptr<Game_Object>>& Wave::create(sf::Vector2f const& player_pos) {
     for(int i{0}; i < wave_number + 1; ++i){
         int spawning_position{random_int(1,4)}; //if to spawn above,below,left or right of screen
         sf::Vector2f pos{0,0};
-        if(spawning_position==1) {          //Above Screen
+        if (spawning_position == 1) {          //Above Screen
             pos = {static_cast<float>(random_int(0, WIDTH)),
                    static_cast<float>(random_int(-200, -135))};
-        }
-        else if(spawning_position==2) {      //Below Screen
-        pos = {static_cast<float>(random_int(0, WIDTH)),
-               static_cast<float>(random_int(HEIGHT, HEIGHT + 100))};
-        }
-        else if(spawning_position==3){      //Left of Screen
+        } else if (spawning_position == 2) {      //Below Screen
+            pos = {static_cast<float>(random_int(0, WIDTH)),
+                   static_cast<float>(random_int(HEIGHT, HEIGHT + 100))};
+        } else if (spawning_position == 3){      //Left of Screen
             pos = {static_cast<float>(random_int(-300, -140)),
                    static_cast<float>(random_int(0, HEIGHT))};
-        }
-        else{                               //Right of Screen
+        } else {                               //Right of Screen
             pos = {static_cast<float>(random_int(WIDTH, WIDTH + 100)),
                    static_cast<float>(random_int(0, HEIGHT))};
         }
-
-
-        //sf::Vector2f pos {static_cast<float>(random_int(-100, WIDTH)),
-        //                  static_cast<float>(random_int(-100, HEIGHT))};
 
         if (static_cast<int>(pos.x) % 2 == 0)
             enemies.emplace_back(std::make_shared<Ranged_Enemy>(pos, 75.f, player_pos));
