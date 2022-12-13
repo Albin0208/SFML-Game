dae98ba509b62525c04c66f9c4f99c72325e73d4
Albin Dahlén <albda746@student.liu.se>
2022-12-13 14:25:29 +0100

    Make enemies spawn outside

--- Wave.cpp
+++ Wave.cpp
@@ -16,24 +16,24 @@ vector<shared_ptr<Game_Object>>& Wave::create(sf::Vector2f const& player_pos) {
     enemies.reserve(wave_number);
 
     for(int i{0}; i < wave_number + 1; ++i){
-        int spawning_position = random_int(1,4); //if to spawn above,below,left or right of screen
+        int spawning_position{random_int(1,4)}; //if to spawn above,below,left or right of screen
         sf::Vector2f pos{0,0};
         if(spawning_position==1) {          //Above Screen
-            pos = {static_cast<float>(random_int(-100, WIDTH)),
-                   static_cast<float>(random_int(-100, 0))};
+            pos = {static_cast<float>(random_int(-150, WIDTH)),
+                   static_cast<float>(random_int(-200, 0))};
         }
-            else if(spawning_position==2) {      //Below Screen
-            pos = {static_cast<float>(random_int(-100, WIDTH)),
-                   static_cast<float>(random_int(HEIGHT, HEIGHT + 100))};
-            }
-            else if(spawning_position==3){      //Left of Screen
-                pos = {static_cast<float>(random_int(-100, 0)),
-                       static_cast<float>(random_int(-100, HEIGHT))};
-            }
-            else{                               //Right of Screen
-                pos = {static_cast<float>(random_int(WIDTH, WIDTH+100)),
-                       static_cast<float>(random_int(-100, HEIGHT))};
-            }
+        else if(spawning_position==2) {      //Below Screen
+        pos = {static_cast<float>(random_int(-150, WIDTH)),
+               static_cast<float>(random_int(HEIGHT, HEIGHT + 200))};
+        }
+        else if(spawning_position==3){      //Left of Screen
+            pos = {static_cast<float>(random_int(-300, -150)),
+                   static_cast<float>(random_int(-200, HEIGHT))};
+        }
+        else{                               //Right of Screen
+            pos = {static_cast<float>(random_int(WIDTH, WIDTH + 200)),
+                   static_cast<float>(random_int(-200, HEIGHT))};
+        }
 
 
         //sf::Vector2f pos {static_cast<float>(random_int(-100, WIDTH)),
